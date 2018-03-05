b82e84effcb7f9c0ed46818cd71757a23fc09659
DansingWithWolfs <dimikson4@gmail.com>
2018-03-05 17:15:27 +0300

    added Java Class ShopFragmentActivity

---
+++ app/src/main/java/com/example/user/garbagecollector/ShopGragmentActivity.java
@@ -0,0 +1,11 @@
+package com.example.user.garbagecollector;
+
+import android.app.Fragment;
+
+/**
+ * Created by User on 05.03.2018.
+ *
+ */
+
+public class ShopGragmentActivity extends Fragment {
+}
