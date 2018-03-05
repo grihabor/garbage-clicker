c66942a2535c75e026c5738d2d3c6acf4f705974
DansingWithWolfs <dimikson4@gmail.com>
2018-03-05 14:28:31 +0300

    Added IntroActivity and hided top strips

--- app/src/main/java/com/example/user/garbagecollector/IntroActivity.java
+++ app/src/main/java/com/example/user/garbagecollector/IntroActivity.java
@@ -1,8 +1,42 @@
 package com.example.user.garbagecollector;
 
-/**
- * Created by User on 05.03.2018.
- */
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
-public class IntroActivity {
+public class IntroActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(getSupportActionBar() != null) getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_intro);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        ConstraintLayout introLayout = findViewById(R.id.introLayout);
+        introLayout.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        }));
+
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/user/garbagecollector/MainActivity.java
+++ app/src/main/java/com/example/user/garbagecollector/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.user.garbagecollector;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        if(getSupportActionBar() != null) getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
