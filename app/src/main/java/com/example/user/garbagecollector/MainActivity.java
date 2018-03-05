f6ed824883b14afa4d7506ea09f946e7b3403be4
DansingWithWolfs <dimikson4@gmail.com>
2018-03-05 16:26:16 +0300

    added onClicks and ID of buttons

--- app/src/main/java/com/example/user/garbagecollector/MainActivity.java
+++ app/src/main/java/com/example/user/garbagecollector/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.garbagecollector;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    public void switchToUpgradeShopFragment(View view) {
+    }
+
+    public void switchAutomationShopFragment(View view) {
+    }
+
+    public void switchToGarbageRecyclingFragment(View view) {
     }
 }
