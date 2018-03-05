ab812d8b311a3c7fe80dc0d0f7771f0d1d71e4ed
RedSnail <Oleg.Demianchenko@gmail.com>
2018-03-05 18:13:05 +0300

    создал файл

---
+++ app/src/main/java/com/example/user/garbagecollector/AutomationFragment.java
@@ -0,0 +1,12 @@
+package com.example.user.garbagecollector;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Олег on 05.03.2018.
+ */
+
+public class AutomationFragment extends Fragment {
+
+}
