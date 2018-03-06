8b407062761c1f173a4eb078f3505be039bddeae
DansingWithWolfs <dimikson4@gmail.com>
2018-03-06 13:48:46 +0300

    UpgradeShopFragment instead UpgradeShopFragmentActivity

--- app/src/main/java/com/example/user/garbagecollector/MainActivity.java
+++ app/src/main/java/com/example/user/garbagecollector/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     //AutomationShopFragment automationShopFragment;
     GarbageRecyclingFragment garbageRecyclingFragment;
-    //UpgradeShopFragmentActivity upgradeShopFragmentActivity;
+    //UpgradeShopFragment upgradeShopFragmentActivity;
     FragmentTransaction fTrans;
 
     @Override
