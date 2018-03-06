c8c9111b2c174621f4f4fd8c29a0fc01f0bc655a
DansingWithWolfs <dimikson4@gmail.com>
2018-03-06 13:41:08 +0300

    added FrameLayout to MainActivity instead Fragment; in MainActivity's onCreate there is an initialization of FrameLayout with GarbageRecyclingFragment now.

---
+++ app/src/main/java/com/example/user/garbagecollector/AutomationShopFragment.java
@@ -0,0 +1,21 @@
+package com.example.user.garbagecollector;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Олег on 05.03.2018.
+ *
+ */
+
+public class AutomationShopFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle bundle) {
+        return inflater.inflate(R.layout.automation_shop_fragment, group);
+    }
+}
--- app/src/main/java/com/example/user/garbagecollector/MainActivity.java
+++ app/src/main/java/com/example/user/garbagecollector/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.user.garbagecollector;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    //AutomationShopFragment automationShopFragment;
+    GarbageRecyclingFragment garbageRecyclingFragment;
+    //UpgradeShopFragmentActivity upgradeShopFragmentActivity;
+    FragmentTransaction fTrans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        garbageRecyclingFragment = new GarbageRecyclingFragment();
+        fTrans = getFragmentManager().beginTransaction();
+        fTrans.add(R.id.fragmentMainLayout, garbageRecyclingFragment);
+        fTrans.commit();
     }
 
     public void switchToUpgradeShopFragment(View view) {
+        
     }
 
     public void switchAutomationShopFragment(View view) {
