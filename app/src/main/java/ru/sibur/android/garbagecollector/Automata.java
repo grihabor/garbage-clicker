2ea01c211b12e75a8f29f28ed6a2c268c93a5031
DansingWithWolfs <dimikson4@gmail.com>
2018-03-08 12:12:48 +0300

    added everything connected with ActumationShopItems

---
+++ app/src/main/java/ru/sibur/android/garbagecollector/Automata.java
@@ -0,0 +1,11 @@
+package ru.sibur.android.garbagecollector;
+
+
+public class Automata {
+    float price;
+    String name;
+    Automata(String nameIN, float priceIn){
+        price = priceIn;
+        name = nameIN;
+    }
+}
