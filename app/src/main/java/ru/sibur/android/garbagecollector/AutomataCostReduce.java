a5ca01ae03785c7befd4ed6d1f006db7876fc2d2
Borodin Gregory <grihabor@mail.ru>
2018-03-18 21:53:50 +0300

    import in the same package

--- app/src/main/java/ru/sibur/android/garbagecollector/AutomataCostReduce.java
+++ app/src/main/java/ru/sibur/android/garbagecollector/AutomataCostReduce.java
@@ -1,7 +1,5 @@
 package ru.sibur.android.garbagecollector;
 
-import ru.sibur.android.garbagecollector.Upgrade;
-
 public class AutomataCostReduce extends Upgrade {
     AutomataCostReduce(String nameIn, int priceIn){
         super(nameIn, priceIn);
