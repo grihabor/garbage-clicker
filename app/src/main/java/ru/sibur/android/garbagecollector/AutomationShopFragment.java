83092b26cced8614d416fb9879e98b7c3471f6fc
Borodin Gregory <grihabor@mail.ru>
2018-03-19 11:09:52 +0300

    docstring

--- app/src/main/java/ru/sibur/android/garbagecollector/AutomationShopFragment.java
+++ app/src/main/java/ru/sibur/android/garbagecollector/AutomationShopFragment.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import java.util.ArrayList;
 
+/**
+ * Фрагмент магазина автоматов
+ */
+
 public class AutomationShopFragment extends ShopFragment {
     @Override
     public void onAttach (Activity activity) {
