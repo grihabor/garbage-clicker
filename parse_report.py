58cb22a84d8c39e9dbd60de578faf9dcb4bdd9e7
Borodin Gregory <grihabor@mail.ru>
2018-03-19 09:26:23 +0300

    try parse json

--- parse_report.py
+++ parse_report.py
@@ -2,12 +2,13 @@ import sys
 import xml.etree.ElementTree as ET
 from xmljson import parker
 import yaml
+import json
 
 
 def main(path):
     root = ET.parse(path).getroot()
     data = parker.data(root)
-    print(yaml.dump(data))
+    print(json.dumps(data, indent=2))
 
 
 if __name__ == '__main__':
