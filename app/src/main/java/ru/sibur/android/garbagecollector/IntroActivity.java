15c208204f7d103be475c27c2a56a84ae2ee9107
Borodin Gregory <grihabor@mail.ru>
2018-03-19 11:20:45 +0300

    fix stylecheck

--- app/src/main/java/ru/sibur/android/garbagecollector/IntroActivity.java
+++ app/src/main/java/ru/sibur/android/garbagecollector/IntroActivity.java
@@ -2,7 +2,6 @@ package ru.sibur.android.garbagecollector;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,40 +23,40 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        if (getSupportActionBar() != null) getSupportActionBar().hide();
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         setContentView(R.layout.activity_intro);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-
         Button playButton = findViewById(R.id.play_button);
-        playButton.setOnClickListener((new View.OnClickListener() {
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(IntroActivity.this, MainActivity.class);
                 startActivity(intent);
-
             }
-        }));
+        });
 
         Button exitButton = findViewById(R.id.exit_button);
-        exitButton.setOnClickListener((new View.OnClickListener() {
+        exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
-        }));
+        });
 
         Button achievementButton = findViewById(R.id.achievement_button);
-        achievementButton.setOnClickListener((new View.OnClickListener() {
+        achievementButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(IntroActivity.this, AchievementActivity.class);
                 startActivity(intent);
 
             }
-        }));
+        });
 
     }
 
