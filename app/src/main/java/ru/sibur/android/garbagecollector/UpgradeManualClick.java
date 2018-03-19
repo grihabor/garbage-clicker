a7695bf6d564acc39a2f981192e98dcd6b8a745e
Gregory <grihabor@gmail.com>
2018-03-19 12:01:16 +0300

    Update UpgradeManualClick.java

--- app/src/main/java/ru/sibur/android/garbagecollector/UpgradeManualClick.java
+++ app/src/main/java/ru/sibur/android/garbagecollector/UpgradeManualClick.java
@@ -1,8 +1,6 @@
 package ru.sibur.android.garbagecollector;
 
 
-import ru.sibur.android.garbagecollector.Upgrade;
-
 public class UpgradeManualClick extends Upgrade {
     UpgradeManualClick(String nameIn, int priceIn) {
         super(nameIn, priceIn);
