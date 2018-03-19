146cbeffc655f4fb10bfc5318e984d76a5087e31
Gregory <grihabor@gmail.com>
2018-03-19 11:46:36 +0300

    import order

--- app/src/main/java/ru/sibur/android/garbagecollector/IntroActivity.java
+++ app/src/main/java/ru/sibur/android/garbagecollector/IntroActivity.java
@@ -2,8 +2,8 @@ package ru.sibur.android.garbagecollector;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
