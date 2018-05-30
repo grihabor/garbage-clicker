fc5bd1c6f2b7f6174e357785cfc30f7f446c36f9
glubshev2001@mail.ru <enterprisenx01>
2018-05-30 13:14:58 +0300

    чертов пробел

--- app/src/main/java/ru/sibur/android/garbagecollector/Automata.java
+++ app/src/main/java/ru/sibur/android/garbagecollector/Automata.java
@@ -23,7 +23,7 @@ public class Automata extends ShopItem {
 
         int price = (int) (
                 basePrice
-                * pow(1.15,count)
+                *pow(1.15,count)
                 *pow(0.85, this.storage.getShopItemCount(Constant.upgradeCountKey(1)))
         );
 
