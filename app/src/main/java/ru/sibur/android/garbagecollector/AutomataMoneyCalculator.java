2707fef63219cfbf12d57d4a797206e4c12de72d
Borodin Gregory <grihabor@gmail.com>
2018-06-03 15:29:52 +0300

    Remove all files

--- app/src/main/java/ru/sibur/android/garbagecollector/AutomataMoneyCalculator.java
+++
@@ -1,9 +0,0 @@
-package ru.sibur.android.garbagecollector;
-
-/**
- * Created by Олег on 23.03.2018.
- */
-
-public interface AutomataMoneyCalculator {
-    int calculateMoney (long timeDifference);
-}
--- app/src/main/java/ru/sibur/android/garbagecollector/OnDBChangeListener.java
+++
@@ -1,9 +0,0 @@
-package ru.sibur.android.garbagecollector;
-
-/**
- * Created by RedSnail on 07.03.2018.
- */
-
-public interface OnDBChangeListener {
-    void onDBChange ();
-}
