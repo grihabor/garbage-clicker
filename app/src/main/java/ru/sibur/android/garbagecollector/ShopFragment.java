108db25bf995e404b399ecc5f0a6a7542fb58915
DansingWithWolfs <33756112+DansingWithWolfs@users.noreply.github.com>
2018-06-08 18:45:16 +0300

    Sounds (#210)
    * добавил худо-будно верстку под normal
    
    * почему в апгрейде все равно показывается мощность
    
    * убогий хардкод звука, в следующем году допилю!
    
    * убогий хардкод звука, в следующем году допилю!
    
    * Update misc.xml
    
    * Update modules.xml
    
    * Update activity_intro.xml
    
    * сильно сжал музыку
    
    * закинул запуск в onStart()
    
    *  deleted old binary file
     Please enter the commit message for your changes. Lines starting
    
    * added settings
    
    * resolved merging conflict

--- app/src/main/java/ru/sibur/android/garbagecollector/ShopFragment.java
+++ app/src/main/java/ru/sibur/android/garbagecollector/ShopFragment.java
@@ -6,6 +6,11 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.ListFragment;
+import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.util.SparseIntArray;
 import android.view.View;
 
 import android.graphics.Color;
@@ -23,6 +28,8 @@ import java.util.HashMap;
 
 import java9.util.stream.IntStream;
 
+import static ru.sibur.android.garbagecollector.Constant.SOUND_QUALITY;
+
 /**
  * Базовый фрагмент для фрагментов магазинов
  */
@@ -30,7 +37,8 @@ import java9.util.stream.IntStream;
 public class ShopFragment extends ListFragment {
 
     private Shop shop;
-
+    private SoundPool soundPool;
+    SparseIntArray soundMap;
 
     void setShop(Shop shop) {
         this.shop = shop;
@@ -45,6 +53,7 @@ public class ShopFragment extends ListFragment {
     @Override
     public void onStart() {
         super.onStart();
+        initializeSoundEffects(shop.context);
         initListView();
     }
 
@@ -77,9 +86,12 @@ public class ShopFragment extends ListFragment {
         ShopItem current = shop.shopItemArray.get(position);
         ValueAnimator animator;
         if(current.tryToBuy(shop.context, shop.storage)) {
+
             animator = (ValueAnimator) AnimatorInflater.loadAnimator(this.getActivity(), R.animator.buying_success);
+            if ( (soundPool != null) && shop.storage.getSoundsShouldBe()) soundPool.play(Constant.SOUND_ONE, Constant.volume, Constant.volume, Constant.PRIORITY, Constant.loop, Constant.rate);
         } else {
             animator = (ValueAnimator) AnimatorInflater.loadAnimator(this.getActivity(), R.animator.buying_fail);
+            if ( (soundPool != null) && shop.storage.getSoundsShouldBe()) soundPool.play(Constant.SOUND_TWO, Constant.volume, Constant.volume, Constant.PRIORITY, Constant.loop, Constant.rate);
         }
 
         animator.addUpdateListener((animation) -> {
@@ -88,6 +100,24 @@ public class ShopFragment extends ListFragment {
         });
 
         animator.start();
+    }
+    public void initializeSoundEffects(Context context) {
+        soundPool = new SoundPool(Constant.MAX_STREAMS, AudioManager.STREAM_MUSIC, Constant.SOUND_QUALITY);
+        if((android.os.Build.VERSION.SDK_INT) == 21){
+            AudioAttributes audioAttrib = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            soundPool = new SoundPool.Builder().setAudioAttributes(audioAttrib).
+                    setMaxStreams(Constant.MAX_STREAMS).build();
+        }
+        else {
+            soundPool = new SoundPool(Constant.MAX_STREAMS, AudioManager.STREAM_MUSIC, SOUND_QUALITY);
+        }
+
+        soundMap = new SparseIntArray(); // быстрый вариант HashMap для андроид
+        soundMap.put(Constant.SOUND_ONE, soundPool.load(context, R.raw.coin, Constant.PRIORITY));
+        soundMap.put(Constant.SOUND_TWO, soundPool.load(context, R.raw.error, Constant.PRIORITY));
     }
 
 }
