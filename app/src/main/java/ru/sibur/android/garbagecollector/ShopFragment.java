f0ce012de8c8cbcdbec66b164637063613df829c
RedSnail <36077611+RedSnail@users.noreply.github.com>
2018-06-08 15:44:26 +0300

    сделал сасную анимацию и вёрстку itro (#209)
    * сделал сасную анимацию и вёрстку itro
    
    * то же самое, только анимации достаются из ресов
    
    * закинул кэш android studio в gitignore
    
    * удалил кэш

--- app/src/main/java/ru/sibur/android/garbagecollector/ShopFragment.java
+++ app/src/main/java/ru/sibur/android/garbagecollector/ShopFragment.java
@@ -1,5 +1,10 @@
 package ru.sibur.android.garbagecollector;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.ListFragment;
 import android.view.View;
 
@@ -7,6 +12,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.TransitionDrawable;
 
+import android.view.ViewPropertyAnimator;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -67,23 +75,19 @@ public class ShopFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         ShopItem current = shop.shopItemArray.get(position);
+        ValueAnimator animator;
         if(current.tryToBuy(shop.context, shop.storage)) {
-            ColorDrawable[] color = {
-                    new ColorDrawable(Color.GREEN),
-                    new ColorDrawable(Color.WHITE)
-            };
-            TransitionDrawable trans = new TransitionDrawable(color);
-            v.setBackground(trans);
-            trans.startTransition(300);
+            animator = (ValueAnimator) AnimatorInflater.loadAnimator(this.getActivity(), R.animator.buying_success);
         } else {
-            ColorDrawable[] color = {
-                    new ColorDrawable(Color.RED),
-                    new ColorDrawable(Color.WHITE)
-            };
-            TransitionDrawable trans = new TransitionDrawable(color);
-            v.setBackground(trans);
-            trans.startTransition(300);
+            animator = (ValueAnimator) AnimatorInflater.loadAnimator(this.getActivity(), R.animator.buying_fail);
         }
+
+        animator.addUpdateListener((animation) -> {
+            int color = (int) animation.getAnimatedValue();
+            v.setBackgroundColor(color);
+        });
+
+        animator.start();
     }
 
 }
